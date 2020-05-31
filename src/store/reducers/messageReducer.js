792d937576ccd7e62735184c55f3bb628d3c0ae6
toshi0228 <etoshi0228@gmail.com>
2020-06-01 01:27:11 +0900

    メッセージルームのメールを時間順に並び変える

--- src/store/reducers/messageReducer.js
+++ src/store/reducers/messageReducer.js
@@ -3,17 +3,13 @@ import _ from 'lodash'
 
 const DEFAULT_STATE = {
   recieveMessage: [],
+  rowDataRecieveMessage: [],
   senderMessage: [],
   roomMessage: [],
 }
 
-// const DEFAULT_STATE = {
-//   isLoading: false,
-//   // トップページのprofileListのデータ
-//   profileList: [],
-//   // profileDetailページのデータ。planListとtagListは最初にmapで作業があるので先に初期値を入れる
-//   profileDetail: { planList: [], tagList: [] },
-// };
+// rowDataRecieveMessageは、メッセージルームのページを開く時、送信したメールと、受信したメールをミックスして、
+// 送信時間ごとにリストするため、時間を加工していないデータを残しておく。
 
 const messageReducer = (state = DEFAULT_STATE, action) => {
   switch (action.type) {
@@ -21,51 +17,62 @@ const messageReducer = (state = DEFAULT_STATE, action) => {
     // 全件のメッセージを送り出すときの処理
     // ==========================================================
     case READ_MESSAGE_EVENTS:
+      // 未加工のデータを残しておく(ルームメッセージの時に、日にちのソートで使う)
+      const rowDataRecieveMessage = _.cloneDeep(action.payload.data)
       // 作成日の加工 "2020-05-24T14:46:01.945895+09:00" -> 2020年10月23
-      // const array = [
-      //   '2020-05-24T14:46:01.945895+09:00',
-      //   '2020-05-30T09:23:32+09:00',
-      //   '2020-05-29T05:55:37.402814+09:00',
-      //   '1943/2/25',
-      //   '1942/6/18',
-      //   '1969/1/14',
-      //   '1940/7/7',
-      //   '1965/5/16',
-      // ]
-      // const ascArray = [...array].sort((a, b) => new Date(a) - new Date(b))
-      // console.log(ascArray)
-
-      action.payload.data.map((message) => {
+      const recieveMessageList = action.payload.data.map((message) => {
         message['createdAt'] = message.createdAt.split('T')[0]
         const time = message['createdAt'].split('-')
         message['createdAt'] = `${time[0]}年${time[1]}月${time[2]}`
         return message
       })
-      return { ...state, recieveMessage: action.payload.data }
+      return { ...state, recieveMessage: recieveMessageList, rowDataRecieveMessage: rowDataRecieveMessage }
 
     // ==========================================================
     // ルームページごとに、メッセージを呼び出すときの処理
     // ==========================================================
     case READ_ROOMMESSAGE_EVENTS:
-      // 自分に送信してくれた人のメッセージを抽出する
-
       const roomMessage = []
-      const senderYorozuId = action.payload
-      _.map(state.recieveMessage, (message) => {
-        if (senderYorozuId === message.senderYorozuId) {
+      // メッセージルームID(urlの一番最後(yozozo))は、送信者のyozozuIDでaction.payloadには、
+      // 送信者のyozozuIDが入っている ex) http://localhost:3000/message/rooms/yozozo
+      const roomMessageId = action.payload
+
+      // 自分に送信してくれた人のメッセージリストから、トークルームのIDと送信者のyorozuIdが同じメッセージを抽出する
+      _.map(state.rowDataRecieveMessage, (message) => {
+        if (roomMessageId === message.senderYorozuId) {
+          roomMessage.push(message)
+        }
+      })
+      // 自分が送信したメッセージリストから、トークルームのIDと受信者のyorozuIdが同じメッセージを抽出する
+      _.map(state.senderMessage, (message) => {
+        if (roomMessageId === message.receiverYorozuId) {
           roomMessage.push(message)
         }
       })
       // pushの注意
       // state.roomMessage.push(message)見たな感じで直接pushは上手く行かない
       // 参照に関しての問題がおこる
-      return { ...state, roomMessage: roomMessage }
+
+      // ルームの中のメッセージをソートする
+      const sortRoomMessage = [...roomMessage].sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
+
+      // 作成日の加工 "2020-05-24T14:46:01.945895+09:00" -> 2020年10月23 16:27
+      sortRoomMessage.map((message) => {
+        const day = message['createdAt'].split('T')[0]
+        const time = message['createdAt'].split('T')[1]
+        const _time = time.split(':')
+        const _day = day.split('-')
+        message['createdAt'] = `${_day[0]}年${_day[1]}月${_day[2]}　${_time[0]}:${_time[1]}`
+        return message
+      })
+
+      return { ...state, roomMessage: sortRoomMessage }
 
     // ==========================================================
     // 自分が送信したメッセージリストの処理
     // ==========================================================
     case READ_MY_SEND_MESSAGE_EVENTS:
-      return { ...state, senderMessage: action.payload }
+      return { ...state, senderMessage: action.payload.data }
 
     // ==========================================================
     // メッセージの送信したときの処理
@@ -73,7 +80,6 @@ const messageReducer = (state = DEFAULT_STATE, action) => {
     case SEND_MESSAGE_EVENT:
       return action.type
     default:
-      // return state
       return state
   }
 }
