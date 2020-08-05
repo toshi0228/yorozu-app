aec57bf1ab1420c2f110c4b28c112233a7b1a544
toshi <etoshi0228@gmail.com>
2020-08-05 20:07:06 +0900

    Amplifyの実験

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,7 +1,9 @@
 const hostList = {
-  localhost: () => 'http://localhost:80',
+  // localhost: () => 'http://localhost:80',
   //   aws: () => 'http://54.178.94.145',
-  //   localhost: () => 'http://54.178.94.145',
+
+  // 本番用
+  localhost: () => 'http://54.178.94.145',
 }
 
 export default hostList
