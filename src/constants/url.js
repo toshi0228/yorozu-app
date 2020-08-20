186c843322353799b63b5a9368542f099f9e021b
toshi <etoshi0228@gmail.com>
2020-08-20 17:59:33 +0900

    https変更

--- src/constants/url.js
+++ src/constants/url.js
@@ -4,9 +4,9 @@ const hostList = {
   // 本番用
   // localhost: () => 'http://54.178.94.145',
   // 試し https
-  // localhost: () => 'https://54.178.94.145',
+  localhost: () => 'https://54.178.94.145',
   // 本番用
-  localhost: () => 'https://yourozu.work',
+  // localhost: () => 'https://yourozu.work',
   // 画像変更
   // localhost: () => 'https://yourozu.s3.amazonaws.com/',
 }
