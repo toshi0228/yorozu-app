5b0a3b17a686766dc03f4e61648c899b66e33c01
toshi <etoshi0228@gmail.com>
2020-08-23 16:08:32 +0900

    検索した時にズームされないように try2

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,12 +1,12 @@
 const hostList = {
-  localhost: () => 'http://localhost:80',
+  // localhost: () => 'http://localhost:80',
   //   aws: () => 'http://54.178.94.145',
   // 本番用
   // localhost: () => 'http://54.178.94.145',
   // 試し https
   // localhost: () => 'https://54.178.94.145',
   // 本番用
-  // localhost: () => 'https://yourozu.work',
+  localhost: () => 'https://yourozu.work',
   // 画像変更
   // localhost: () => 'https://yourozu.s3.amazonaws.com/',
 }
