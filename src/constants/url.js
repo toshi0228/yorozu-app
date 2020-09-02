58d3d90550c522ee9e9515b7a8124422595139c2
toshi <etoshi0228@gmail.com>
2020-09-02 13:18:28 +0900

    URL 変更

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,7 +1,7 @@
 const hostList = {
   // localhost: () => 'http://localhost:80',
   //   aws: () => 'http://54.178.94.145',
-  // 本番用
+  // 本番用apiリクエスト
   localhost: () => 'https://yourozu.work',
 }
 
