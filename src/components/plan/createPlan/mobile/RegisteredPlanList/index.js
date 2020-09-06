44edaf63d738eb488ecc0f5ad3b176b90380bf2f
toshi <etoshi0228@gmail.com>
2020-09-06 15:45:06 +0900

    モバイル用にプランの削除を追加

--- src/components/plan/createPlan/mobile/RegisteredPlanList/index.js
+++ src/components/plan/createPlan/mobile/RegisteredPlanList/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Col, Row } from 'antd'
+import { Col, Row, Button, Popconfirm } from 'antd'
 
 import styles from './index.module.scss'
 
 // action
-import { readEditPlan, readyCreatePlan } from '../../../../../store/actions/plan'
+import { readEditPlan, readyCreatePlan, removePlan } from '../../../../../store/actions/plan'
 
 // ======================================================================
 // 新規で登録するプランリストがここに入る モバイル用
 // ======================================================================
 
 // planDataは、CreatePlanPageから渡ってくる
-const RegisteredPlanList = ({ planData, readEditPlanEvent, readyCreatePlanEvent }) => {
+const RegisteredPlanList = ({ planData, readEditPlanEvent, readyCreatePlanEvent, deletePlanEvent }) => {
   // ====================================================================
   // 登録済みのプランを選んだ時に処理 選択することによって右側の編集項目がでる
   // ====================================================================
@@ -28,18 +28,48 @@ const RegisteredPlanList = ({ planData, readEditPlanEvent, readyCreatePlanEvent 
   }
 
   // ====================================================================
+  // 削除ボタンを押した時の処理
+  // ====================================================================
+  const deletePlan = (plan) => {
+    deletePlanEvent(plan.id)
+  }
+
+  // ====================================================================
   //  登録済みのプランリストを表示する
   // ====================================================================
   const planList = planData.map((plan, index) => {
     return (
       <Row className={styles.planCard} key={index}>
+        {/* 画像 */}
         <Col onClick={() => editPlan(plan)}>
           <img src={plan.image} className={styles.planImage} />
         </Col>
-        <Col style={{ marginTop: 4 }}>
-          <h4 style={{ margin: 0 }}>{plan.title}</h4>
-        </Col>
-        <Col style={{ fontSize: 12 }}>{`¥${plan.price}円`}</Col>
+
+        <Row>
+          {/* タイトル/ 値段 18/ 24 */}
+          <Col span={18} style={{ marginTop: 4 }}>
+            {/* タイトル */}
+            <h4 style={{ margin: 0 }}>{plan.title}</h4>
+
+            {/* 値段 */}
+            <div style={{ fontSize: 12 }}>{`¥${plan.price}円`}</div>
+          </Col>
+
+          {/* 削除ボタン 20/ 24  */}
+          <Col offset={20} style={{ marginTop: 4 }}>
+            <Popconfirm
+              placement="topLeft"
+              title="プランを削除してよろしいですか？"
+              onConfirm={() => deletePlan(plan)}
+              okText="はい"
+              cancelText="いいえ"
+            >
+              <Button block={true} size="small">
+                削除
+              </Button>
+            </Popconfirm>
+          </Col>
+        </Row>
       </Row>
     )
   })
@@ -65,6 +95,8 @@ const mapDispatchToProps = (dispatch) => ({
   readEditPlanEvent: (planId) => dispatch(readEditPlan(planId)),
   // 新規登録の作業なので、右側の入力項目を空白にする
   readyCreatePlanEvent: () => dispatch(readyCreatePlan()),
+  // プランを削除する
+  deletePlanEvent: (planId) => dispatch(removePlan(planId)),
 })
 
 export default connect(null, mapDispatchToProps)(RegisteredPlanList)
