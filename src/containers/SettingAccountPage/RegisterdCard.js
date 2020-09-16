7190eafdfca4ec21f3996400f2f7d7b1f7d4a128
toshi <etoshi0228@gmail.com>
2020-09-16 22:00:06 +0900

    クレジットカードの登録ページ追加

---
+++ src/containers/SettingAccountPage/RegisterdCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+// クレジットカードの登録ページ
+// ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+
+const RegisterdCard = () => {
+  return <h1>カードページ作り中</h1>
+}
+
+export default RegisterdCard
